fd30e96f422a0fa0357108c61b0d3ecb05c577eb
aliafi <alireza.yousefi98@gmail.com>
2021-04-08 22:21:52 +0430

    update v4
    update v4

--- trangellzarinpal.php
+++ trangellzarinpal.php
@@ -80,10 +80,10 @@ class PlgPaymentTrangellZarinpal extends JPlugin
 		
 
 		try {
-			  $client = new SoapClient('https://www.zarinpal.com/pg/services/WebGate/wsdl', ['encoding' => 'UTF-8']); 	
+			 // $client = new SoapClient('https://www.zarinpal.com/pg/services/WebGate/wsdl', ['encoding' => 'UTF-8']);
 			//$client = new SoapClient('https://sandbox.zarinpal.com/pg/services/WebGate/wsdl', ['encoding' => 'UTF-8']); // for local
 
-			$result = $client->PaymentRequest(
+			/*$result = $client->PaymentRequest(
 				[
 				'MerchantID' => $this->params->get('merchant_id',''),
 				'Amount' => $Amount,
@@ -92,28 +92,51 @@ class PlgPaymentTrangellZarinpal extends JPlugin
 				'Mobile' => $Mobile,
 				'CallbackURL' => $CallbackURL,
 				]
-			);
+			);*/
+			/////////////////////////////////////////////////////////////////////////
+            $data = array("merchant_id" => $this->params->get('merchant_id',''),
+                "amount" => $Amount,
+                "callback_url" => $CallbackURL,
+                "description" => $Description,
+                "metadata" => [ "email" => "0","mobile"=>"0"],
+            );
+            $jsonData = json_encode($data);
+            $ch = curl_init('https://api.zarinpal.com/pg/v4/payment/request.json');
+            curl_setopt($ch, CURLOPT_USERAGENT, 'ZarinPal Rest Api v1');
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                'Content-Type: application/json',
+                'Content-Length: ' . strlen($jsonData)
+            ));
+
+            $result = curl_exec($ch);
+            $err = curl_error($ch);
+            $result = json_decode($result, true, JSON_PRETTY_PRINT);
+            curl_close($ch);
+            /// /////////////////////////////////////////////////////////////////////
 			
-			$resultStatus = abs($result->Status); 
-			if ($resultStatus == 100) {
+			//$resultStatus = abs($result->Status);
+			if ($result['data']['code'] == 100) {
 			
 			// Header('Location: https://www.zarinpal.com/pg/StartPay/'.$result->Authority); 
-				if ($this->params->get('zaringate', '') == 0){
-					$vars->action_url = 'https://www.zarinpal.com/pg/StartPay/'.$result->Authority;
-				}
-				else {
-					$vars->action_url = 'https://www.zarinpal.com/pg/StartPay/'.$result->Authority.'/ZarinGate';
-				}
+			//	if ($this->params->get('zaringate', '') == 0){
+					//$vars->action_url = 'https://www.zarinpal.com/pg/StartPay/'.$result->Authority;
+				//}
+				//else {
+					$vars->action_url = 'https://www.zarinpal.com/pg/StartPay/'.$result['data']["authority"];
+				//}
 				$html = $this->buildLayout($vars);
 
 				return $html;
 			} else {
 				$msg= plgPaymentTrangellZarinpalHelper::getGateMsg('error'); 
 				$link = JRoute::_(JUri::root().	'index.php?option=com_jgive&task=donations.cancel&processor=trangellzarinpal&order_id='.$vars->order_id,false);
-				$app->redirect($link, '<h2>'.$msg.'  خطای: '.$resultStatus.'</h2>', $msgType='Error'); 
+				$app->redirect($link, '<h2>'.$msg.'  خطای: '.$result['errors']['code'].'</h2>', $msgType='Error');
 			}
 		}
-		catch(\SoapFault $e) {
+		catch(Exception $e) {
 			$msg= plgPaymentTrangellZarinpalHelper::getGateMsg('error'); 
 			$link = JRoute::_(JUri::root().	'index.php?option=com_jgive&task=donations.cancel&processor=trangellzarinpal&order_id='.$vars->order_id,false);
 			$app->redirect($link, '<h2>'.$msg.'</h2>', $msgType='Error'); 
@@ -124,29 +147,45 @@ class PlgPaymentTrangellZarinpal extends JPlugin
 	public function onTP_Processpayment($data, $vars = array()) {
 		$app	= JFactory::getApplication();		
 		$jinput = $app->input;
-		$Authority = $jinput->get->get('Authority', '0', 'INT');
+		$Authority = $jinput->get->get('Authority', 'STRING');
 		$status = $jinput->get->get('Status', '', 'STRING');
 			
 			if (checkHack::checkString($status)){
 				if ($status == 'OK') {
 					try {
-						$client = new SoapClient('https://www.zarinpal.com/pg/services/WebGate/wsdl', ['encoding' => 'UTF-8']); 
+						//$client = new SoapClient('https://www.zarinpal.com/pg/services/WebGate/wsdl', ['encoding' => 'UTF-8']);
 						//$client = new SoapClient('https://sandbox.zarinpal.com/pg/services/WebGate/wsdl', ['encoding' => 'UTF-8']); // for local
 
-						$result = $client->PaymentVerification(
+						/*$result = $client->PaymentVerification(
 							[
 								'MerchantID' =>$this->params->get('merchant_id',''),
 								'Authority' => $Authority,
 								'Amount' => round($vars->amount/10,0),
 							]
-						);
-						$resultStatus = abs($result->Status); 
-						if ($resultStatus == 100) {
+						);*/
+						////////////////////////////////////////////////////////////////////////////////
+                        $data = array("merchant_id" => $this->params->get('merchant_id',''), "authority" => $Authority, "amount" => round($vars->amount/10,0));
+                        $jsonData = json_encode($data);
+                        $ch = curl_init('https://api.zarinpal.com/pg/v4/payment/verify.json');
+                        curl_setopt($ch, CURLOPT_USERAGENT, 'ZarinPal Rest Api v4');
+                        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+                        curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+                        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                            'Content-Type: application/json',
+                            'Content-Length: ' . strlen($jsonData)
+                        ));
+                        $result = curl_exec($ch);
+                        curl_close($ch);
+                        $result = json_decode($result, true);
+                        /// ///////////////////////////////////////////////////////////////////////////
+						//$resultStatus = abs($result->Status);
+						if ($result['data']['code'] == 100) {
 							
-							$msg= plgPaymentTrangellZarinpalHelper::getGateMsg($resultStatus); 
-							JFactory::getApplication()->enqueueMessage('<h2>'.$msg.'</h2>'.'<h3>'. $result->RefID .'شماره پیگری ' .'</h3>', 'Message');
+							$msg= plgPaymentTrangellZarinpalHelper::getGateMsg($result['data']['code']);
+							JFactory::getApplication()->enqueueMessage('<h2>'.$msg.'</h2>'.'<h3>'. $result['data']['ref_id'] .'شماره پیگری ' .'</h3>', 'Message');
 							
-							plgPaymentTrangellZarinpalHelper::saveComment($this->params->get('plugin_name'), str_replace('JGOID-','',$vars->order_id), $result->RefID .'شماره پیگری ');
+							plgPaymentTrangellZarinpalHelper::saveComment($this->params->get('plugin_name'), str_replace('JGOID-','',$vars->order_id), $result['data']['ref_id'] .'شماره پیگری ');
 							$result                 = array(
 							'transaction_id' => '',
 							'order_id' => $vars->order_id,
@@ -160,13 +199,13 @@ class PlgPaymentTrangellZarinpal extends JPlugin
 							return $result;
 						} 
 						else {
-							$msg= plgPaymentTrangellZarinpalHelper::getGateMsg($resultStatus); 
+							$msg= plgPaymentTrangellZarinpalHelper::getGateMsg($result['errors']['code']);
 							$link = JRoute::_(JUri::root().	'index.php?option=com_jgive&task=donations.cancel&processor=trangellzarinpal&order_id='.$vars->order_id,false);
 							$app->redirect($link, '<h2>'.$msg.'</h2>', $msgType='Error'); 
 							return false;
 						}
 					}
-					catch(\SoapFault $e) {
+					catch(Exception $e) {
 						$msg= plgPaymentTrangellZarinpalHelper::getGateMsg('error'); 
 						$link = JRoute::_(JUri::root().	'index.php?option=com_jgive&task=donations.cancel&processor=trangellzarinpal&order_id='.$vars->order_id,false);
 						$app->redirect($link, '<h2>'.$msg.'</h2>', $msgType='Error'); 
